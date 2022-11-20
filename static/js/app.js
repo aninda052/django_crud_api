72da3ddf6b3c6d3d6854db63640a30c28c41abe4
aninda052 <anindahalder052@gmail.com>
2022-11-20 22:45:23 +0600

    pagination added

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-app = angular.module("demoapp", []);
+app = angular.module("demoapp",  ['ui.bootstrap']);
 
 app.controller("authtrl",  function ($scope, $http){
 
@@ -72,6 +72,8 @@ app.controller("eventctrl",  function ($scope, $http){
 
     $scope.showEventList = true;
     $scope.showEventCreateUpdate = false;
+    $scope.pageIndex = 1;
+    $scope.numberOfEvenentPerPage = $scope.numberOfEvenentPerPage != undefined ? $scope.numberOfEvenentPerPage:5
     getEventList()
 
     $scope.eventCreatePage = function (){
@@ -85,14 +87,16 @@ app.controller("eventctrl",  function ($scope, $http){
         $scope.showEventList = false;
 
     };
+
     $scope.eventListPage = function (){
         $scope.showEventList =  true;
         $scope.showEventCreateUpdate = false;
-        // getEventList()
     };
 
-
-    $scope.eventlist = getEventList
+    $scope.getEventlistByPageSize = function (){
+        $scope.pageIndex = 1;
+        getEventList()
+    }
 
     $scope.createEvent = function (){
         $http.post(
@@ -169,12 +173,39 @@ app.controller("eventctrl",  function ($scope, $http){
         $scope.showUpdateEventBtn = true;
     }
 
+    $scope.updateEvent =  function (){
+            $http.put(
+                `http://127.0.0.1:8000/api/events/${$scope.eventId}`,
+                {
+                    "name":$scope.eventName,
+                    "location":$scope.location,
+                    "date":$scope.date,
+                },
+                {
+                    "headers": {
+                        "Authorization": 'Bearer ' + localStorage.getItem('token'),
+                    },
+                }
 
+            ).then(function (data){
+                $scope.eventListPage()
+                alert(data.data.massage)
+            }).catch(function (error){
+                // if (error.status==401){
+                //
+                // }
+                alert(error.data.detail)
+            })
+        };
+
+    $scope.getEventlistByPageIndex = function (){
+        getEventList()
+    }
 
     function getEventList(){
 
         $http.get(
-            "http://127.0.0.1:8000/api/events?number=1&size=5",
+            `http://127.0.0.1:8000/api/events?number=${$scope.pageIndex}&size=${$scope.numberOfEvenentPerPage}`,
             {
                 "headers": {
                     "Authorization": 'Bearer ' + localStorage.getItem('token'),
@@ -182,7 +213,9 @@ app.controller("eventctrl",  function ($scope, $http){
             }
         ).then(function (data){
             $scope.events = data.data.results
-            // $('#dtBasicExample').DataTable();
+            $scope.totalEventCount = data.data.count;
+            $scope.showingEventsFrom = ($scope.pageIndex-1) * $scope.numberOfEvenentPerPage;
+            $scope.showingEventsTo = $scope.showingEventsFrom + data.data.results.length;
 
 
         }).catch(function (error){
@@ -193,30 +226,7 @@ app.controller("eventctrl",  function ($scope, $http){
         })
     };
 
-    $scope.updateEvent =  function (){
-        $http.put(
-            `http://127.0.0.1:8000/api/events/${$scope.eventId}`,
-            {
-                "name":$scope.eventName,
-                "location":$scope.location,
-                "date":$scope.date,
-            },
-            {
-                "headers": {
-                    "Authorization": 'Bearer ' + localStorage.getItem('token'),
-                },
-            }
+  });
 
-        ).then(function (data){
-            $scope.eventListPage()
-            alert(data.data.massage)
-        }).catch(function (error){
-            // if (error.status==401){
-            //
-            // }
-            alert(error.data.detail)
-        })
-    };
-})
 
 
